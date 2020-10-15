83124c3e4a78453a8e33635d33bd6df3ddb242c3
maualvm@gmail.com <maualvm@gmail.com>
2020-10-15 15:26:12 -0500

    Cambiar seeder tabla evaluacion a su propia clase

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,15 +14,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        DB::table('evaluacion')->insert([
-            'nombreEvaluacion' => 'Inicio de la jornada',
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-        ]);
-        DB::table('evaluacion')->insert([
-            'nombreEvaluacion' => 'Final de la jornada',
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-        ]);
     }
 }
