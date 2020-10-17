957dd17e6417bfedfc666dfa608f1d27e5eb4f9f
maualvm@gmail.com <maualvm@gmail.com>
2020-10-17 16:10:43 -0500

    Optimizar seeder tabla opcion_evaluacion

--- backend/database/seeders/OpcionEvaluacionTableSeeder.php
+++ backend/database/seeders/OpcionEvaluacionTableSeeder.php
@@ -16,44 +16,49 @@ class OpcionEvaluacionTableSeeder extends Seeder
      */
     public function run()
     {
-        $jornada = DB::table('evaluacion')->get();
-        $preguntas = DB::table('evaluaciones_preguntas')->get();
-        $respuestaOfrecida = DB::table('evaluaciones_respuestas_ofrecidas')->get();
-
-        //Insertar todas las respuestas 'Si' a las evaluaciones del inicio de la jornada
-        foreach($preguntas as $pregunta) {
-            OpcionEvaluacion::create([
-                'idEvaluacion' => $jornada[0]->idEvaluacion,
-                'idEvaluacionPregunta' => $pregunta->idEvaluacionPregunta,
-                'idRespuestaOfrecida' => $respuestaOfrecida[0]->idRespuestaOfrecida,
-            ]);
+        $jornada = DB::table('evaluacion')->get(); // Obtiene los tipos de jornada (Inicio de la jornada/Fin de la jornada)
+        $preguntas = DB::table('evaluaciones_preguntas')->get(); // Obtiene todas las preguntas (9)
+        $respuestaOfrecida = DB::table('evaluaciones_respuestas_ofrecidas')->get(); // Obtiene las respuestas ofrecidas (Sí/No)
+        
+        $modulador = true; // Controla el cambio de respuesta entre 'Sí' y 'No'
+        
+        //Insertar todas las respuestas a las evaluaciones del inicio de la jornada
+        for($i = 0; $i < 2*count($preguntas); $i++) {
+            if($i > (2*count($preguntas)/2)-1) {
+                OpcionEvaluacion::create([
+                    'idEvaluacion' => $jornada[0]->idEvaluacion,
+                    'idEvaluacionPregunta' => $preguntas[$i-9]->idEvaluacionPregunta,
+                    'idRespuestaOfrecida' => $modulador  ? $respuestaOfrecida[0]->idRespuestaOfrecida : $respuestaOfrecida[1]->idRespuestaOfrecida,
+                ]);
+            }
+            else {
+                OpcionEvaluacion::create([
+                    'idEvaluacion' => $jornada[0]->idEvaluacion,
+                    'idEvaluacionPregunta' => $preguntas[$i]->idEvaluacionPregunta,
+                    'idRespuestaOfrecida' => $modulador  ? $respuestaOfrecida[0]->idRespuestaOfrecida : $respuestaOfrecida[1]->idRespuestaOfrecida,
+                ]);
+            }
+            $modulador = !$modulador;
         }
         
-        //Insertar todas las respuestas 'No' a las evaluaciones del inicio de la jornada
-        foreach($preguntas as $pregunta) {
-            OpcionEvaluacion::create([
-                'idEvaluacion' => $jornada[0]->idEvaluacion,
-                'idEvaluacionPregunta' => $pregunta->idEvaluacionPregunta,
-                'idRespuestaOfrecida' => $respuestaOfrecida[1]->idRespuestaOfrecida,
-            ]);
-        }
-
-        //Insertar todas las respuestas 'Si' a las evaluaciones del fin de la jornada
-        foreach($preguntas as $pregunta) {
-            OpcionEvaluacion::create([
-                'idEvaluacion' => $jornada[1]->idEvaluacion,
-                'idEvaluacionPregunta' => $pregunta->idEvaluacionPregunta,
-                'idRespuestaOfrecida' => $respuestaOfrecida[0]->idRespuestaOfrecida,
-            ]);
-        }
-
-        //Insertar todas las respuestas 'No' a las evaluaciones del fin de la jornada
-        foreach($preguntas as $pregunta) {
-            OpcionEvaluacion::create([
-                'idEvaluacion' => $jornada[1]->idEvaluacion,
-                'idEvaluacionPregunta' => $pregunta->idEvaluacionPregunta,
-                'idRespuestaOfrecida' => $respuestaOfrecida[1]->idRespuestaOfrecida,
-            ]);
+        //Insertar todas las respuestas a las evaluaciones del fin de la jornada
+        $modulador = true; //Reinicia a true para empezar desde 'Sí' (en caso de que hayan más o menos preguntas en un futuro)
+        for($i = 0; $i < 2*count($preguntas); $i++) {
+            if($i > (2*count($preguntas)/2)-1) {
+                OpcionEvaluacion::create([
+                    'idEvaluacion' => $jornada[1]->idEvaluacion,
+                    'idEvaluacionPregunta' => $preguntas[$i-9]->idEvaluacionPregunta,
+                    'idRespuestaOfrecida' => $modulador  ? $respuestaOfrecida[0]->idRespuestaOfrecida : $respuestaOfrecida[1]->idRespuestaOfrecida,
+                ]);
+            }
+            else {
+                OpcionEvaluacion::create([
+                    'idEvaluacion' => $jornada[1]->idEvaluacion,
+                    'idEvaluacionPregunta' => $preguntas[$i]->idEvaluacionPregunta,
+                    'idRespuestaOfrecida' => $modulador  ? $respuestaOfrecida[0]->idRespuestaOfrecida : $respuestaOfrecida[1]->idRespuestaOfrecida,
+                ]);
+            }
+            $modulador = !$modulador;
         }
     }
 }
