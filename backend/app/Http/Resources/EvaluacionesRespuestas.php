22c7a707b85c1d0f1c5ca790e6855d1f25a62e5d
maualvm@gmail.com <maualvm@gmail.com>
2020-10-20 12:24:59 -0500

    Renombrar EvaluacionesRespuestasCollection a EvaluacionesRespuestas

--- backend/app/Http/Resources/EvaluacionesRespuestas.php
+++ backend/app/Http/Resources/EvaluacionesRespuestas.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class EvaluacionesRespuestasCollection extends JsonResource
+class EvaluacionesRespuestas extends JsonResource
 {
     /**
      * Transform the resource into an array.
