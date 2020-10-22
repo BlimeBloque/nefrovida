e1621fe10de5af24f94fd28b7ce46a13bceb3638
RHazaelJimAr <randy_hazael_10@live.com.mx>
2020-10-22 16:17:20 -0500

    Icono Consulta

--- frontend/src/components/SeccionConsultaMedica.js
+++ frontend/src/components/SeccionConsultaMedica.js
@@ -1,21 +1,21 @@
 import { Typography, makeStyles, Paper } from '@material-ui/core'
 import React, { useState, useEffect } from 'react'
-import AddIcon from '@material-ui/icons/Add'; 
+import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
-import RestaurantIcon from '@material-ui/icons/Restaurant';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 
 
 const useStyle = makeStyles(theme => ({
-    flex:{
+    flex: {
         display: "flex",
         justifyContent: "space-evenly",
     },
-    flexContent:{
+    flexContent: {
         marginBottom: theme.spacing(3),
-    },paper: {
+    }, paper: {
         height: 140,
         width: 100,
         textAlign: "center",
@@ -29,47 +29,48 @@ const SeccionConsultaMedica = (props) => {
     const classes = useStyle();
     const [consultas, setConsultas] = useState([]);
 
-    useEffect ( () => {
-        axios.get('http://localhost:8000/api/consultaMedica/beneficiario/'+props.idBeneficiario)
-            .then(res => { setConsultas(res.data)
-                })
-                    .catch((e) => {
-                    console.log(e)
-                });
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/consultaMedica/beneficiario/' + props.idBeneficiario)
+            .then(res => {
+                setConsultas(res.data)
+            })
+            .catch((e) => {
+                console.log(e)
+            });
     }, []);
 
-    return(
+    return (
         <div>
-        {console.log(consultas)}
+            {console.log(consultas)}
             <div className={classes.flex}>
-                <Typography className={classes.flexContent} style={{margin: "10px 0px 0px 0px"}} variant="h6">
-                <strong>Consultas Médicas</strong>
+                <Typography className={classes.flexContent} style={{ margin: "10px 0px 0px 0px" }} variant="h6">
+                    <strong>Consultas Médicas</strong>
                 </Typography>
-                <Fab className={classes.flexContent} color="primary" onClick={() => props.history.push("/beneficiarios/"+props.idBeneficiario+"/agregarConsultaMedica")}>
-                    <AddIcon/>
+                <Fab className={classes.flexContent} color="primary" onClick={() => props.history.push("/beneficiarios/" + props.idBeneficiario + "/agregarConsultaMedica")}>
+                    <AddIcon />
                 </Fab>
             </div>
 
-                <Grid container justify="center" spacing={4}>
+            <Grid container justify="center" spacing={4}>
                 {
-                    consultas.length ? 
+                    consultas.length ?
                         consultas.map((consulta) => (
-                        <Grid key={consulta.idConsultaMedica} item>
-                            <Paper className={classes.paper}>
-                                <IconButton aria-label="Consultar" className={classes.margin} onClick={() => props.history.push("/consultaMedica/"+consulta.idConsultaMedica)}>
-                                    <RestaurantIcon fontSize="large" />
-                                </IconButton>
+                            <Grid key={consulta.idConsultaMedica} item>
+                                <Paper className={classes.paper}>
+                                    <IconButton aria-label="Consultar" className={classes.margin} onClick={() => props.history.push("/consultaMedica/" + consulta.idConsultaMedica)}>
+                                        <LocalHospitalIcon fontSize="large" />
+                                    </IconButton>
                                 Consulta del: {consulta.created_at}
-                            </Paper>
-                        </Grid>
-                        
+                                </Paper>
+                            </Grid>
+
                         ))
-                
-                    :
-                    <Typography variant="body">No hay consultas médicas registradas para este beneficiario</Typography>
-                
+
+                        :
+                        <Typography variant="body">No hay consultas médicas registradas para este beneficiario</Typography>
+
                 }
-                </Grid>
+            </Grid>
         </div>
     );
 }
