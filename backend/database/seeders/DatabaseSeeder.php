fdd36b1b7667928274e947efa0d2451fa7ac1ff8
A01208320 <A01208320@itesm.mx>
2020-10-26 00:18:10 -0600

    Seeders
    Automatizar seeders

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,15 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run() {
+        $this->call([
+            EscolaridadesSeeder::class,
+            BeneficiariosSeeder::class,
+            EvaluacionTableSeeder::class,
+            JornadasTableSeeder::class,
+            EvaluacionesPreguntasTableSeeder::class,
+            EvaluacionesRespuestasTableSeeder::class,
+            OpcionEvaluacionTableSeeder::class,
+        ]);
     }
 }
