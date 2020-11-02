1b75c7f38e61e6f85f91cb7f2a3aad25dad7954b
maualvm@gmail.com <maualvm@gmail.com>
2020-11-02 11:39:01 -0600

    Arreglar onChange del RadioGroup

--- frontend/src/views/Evaluaciones/EditarEvaluacionValores.js
+++ frontend/src/views/Evaluaciones/EditarEvaluacionValores.js
@@ -61,13 +61,17 @@ const respuestasFin = {
 function EditarEvaluacionValores(props) {
     const { history } = props;
     const classes = useStyle();
-    const [valoresInicio, setValoresInicio] = useState();
-    const [valoresFin, setValoresFin] = useState();
-    const [idEvaluacion] = useState(window.location.pathname.split("/").pop() === 'detalleEvaluacionesInicio' ? 1 : 2);
+    const [opciones, setOpciones] = useState([])
+    const [valoresInicio, setValoresInicio] = useState([]);
+    const [valoresFin, setValoresFin] = useState([]);
+    const [idEvaluacion] = useState(window.location.pathname.split("/").pop() === 'editarEvaluacionInicio' ? 1 : 2);
     const [disabled, setDisabled] = useState(false);
 
     const handleChange = (event) => {
+        console.log(valoresInicio)
         const idRespuesta = event.target.parentElement.parentElement.parentElement.parentElement.id; 
+        console.log(idRespuesta)
+        console.log(event.target.value)
         if(idEvaluacion == 1) {
             setValoresInicio({
                 ...valoresInicio,
@@ -87,7 +91,8 @@ function EditarEvaluacionValores(props) {
         if(idEvaluacion == 1) {
             axios.get('http://localhost:8000/api/detallesEvaluacionesInicio/'+ props.idBeneficiario)
             .then(res => { 
-                setValoresInicio(res.data)
+                setOpciones(res.data)
+                console.log(res.data)
             })
             .catch((e) => {
                 console.log(e)
@@ -95,7 +100,8 @@ function EditarEvaluacionValores(props) {
         } else {
             axios.get('http://localhost:8000/api/detallesEvaluacionesFin/'+ props.idBeneficiario)
             .then(res => { 
-                setValoresFin(res.data)
+                setOpciones(res.data)
+                console.log(res.data)
             })
             .catch((e) => {
                 console.log(e)
@@ -105,9 +111,47 @@ function EditarEvaluacionValores(props) {
 
     }, []);
 
+    const handleSubmit = (event) => {
+        setDisabled(true);
+        let valueRespuesta = {};
+        let arrayForm = [];
+
+        let i = idEvaluacion == 1 ? 1 : 10 // Decidir en donde empieza el contador del ciclo for
+        /* 
+            Todos los operadores ternarios aquí los utilicé para no hacer dos ciclos for.
+            El que está dentro de la declaración del ciclo delimita si termina en 10 o 19
+            El que está en el valor de respuestasPosibles agrega el valor sacado del arreglo correspondiente (valueInicio/valueFin)
+        */
+        for (i; i < (idEvaluacion == 1 ? 10 : 19); i++) {
+            valueRespuesta = {
+                idOpcionEvaluacion: i,
+                idBeneficiario: props.match.params.idBeneficiario,
+                otraRespuesta: null,
+                respuestasPosibles: (idEvaluacion == 1 ? valoresInicio[i] : valoresFin[i])
+            }
+            if(idEvaluacion == 1) arrayForm[i] = valueRespuesta;
+            else arrayForm[i-9] = valueRespuesta;
+        }
+        console.log(arrayForm)
+
+        /* for (let i = 1; i < 10; i++) {
+            axios.post('http://localhost:8000/api/evaluacion', arrayForm[i], {headers: {"Accept": "application/json"}})
+                .then(res => {
+                    props.history.push("/beneficiarios/"+props.match.params.idBeneficiario+"?agregarEvaluacion=1");
+
+                })
+                .catch(err => {
+                    console.log(err)
+                    props.history.push("/beneficiarios/"+props.match.params.idBeneficiario+"?agregarEvaluacion=0");
+                });
+        } */
+        
+    }
+
     return (
         <div className={classes.root}>
             <CssBaseline />
+            
             <Typography variant="h4" className={classes.centerItem}>Editar Formulario de Evaluación</Typography>
             {
                 // Definir Título de formulario
@@ -123,11 +167,14 @@ function EditarEvaluacionValores(props) {
             <Divider className={classes.divider}/>
             <form className={classes.form}>
                 {
-                    valoresInicio ? // Cuando regrese el json, mapearlo
-                        valoresInicio.map((valor) => (
-                            <>
+                        opciones.map((valor) => (
                                 <FormControl component="fieldset" style={{display: 'block'}}>
-                                    <Typography variant="h6">{valor.idOpcionEvaluacion}.- {valor.evaluacionPregunta}</Typography>
+                                    {
+                                        idEvaluacion == 1 ?
+                                        <Typography variant="h6">{valor.idOpcionEvaluacion}.- {valor.evaluacionPregunta}</Typography> :
+                                        <Typography variant="h6">{valor.idOpcionEvaluacion-9}.- {valor.evaluacionPregunta}</Typography>
+                                    }
+
                                     <RadioGroup 
                                         row 
                                         aria-label="respuestas" 
@@ -135,47 +182,23 @@ function EditarEvaluacionValores(props) {
                                         onChange={handleChange}
                                         id={valor.idOpcionEvaluacion}
                                         style={{textAlign: 'center'}}   
-                                        value={valor.respuestasPosibles} 
+                                        defaultValue={valor.respuestasPosibles} 
                                     >
                                         <FormControlLabel value="Sí" control={<Radio required />} label="Sí" />
                                         <FormControlLabel value="No" control={<Radio required />} label="No" />
                                     </RadioGroup>
                                 </FormControl>
-                            </>
-                        )) :
-                    <div/>
+                        )) 
+                    
                 }
-                {
-                    valoresFin ?
-                    valoresFin.map((valor) => (
-                        <>
-                            <FormControl component="fieldset" style={{display: 'block'}}>
-                                <Typography variant="h6">{valor.idOpcionEvaluacion}.- {valor.evaluacionPregunta}</Typography>
-                                <RadioGroup 
-                                    row 
-                                    aria-label="respuestas" 
-                                    name="pregunta-respuestas" 
-                                    onChange={handleChange}
-                                    id={valor.idOpcionEvaluacion}
-                                    style={{textAlign: 'center'}}   
-                                    value={valor.respuestasPosibles} 
-                                >
-                                    <FormControlLabel value="Sí" control={<Radio required />} label="Sí" />
-                                    <FormControlLabel value="No" control={<Radio required />} label="No" />
-                                </RadioGroup>
-                            </FormControl>
-                        </>
-                    )) :                  
-                    <div/>
-                }
+               
                 <div className={classes.formItems}>
-                        
                         <Button color="default" className={classes.back} onClick={() => history.push('/beneficiarios/'+props.match.params.idBeneficiario)}>Cancelar</Button>
                         <Button 
                             disabled={disabled} 
                             variant="contained" 
                             color="primary" 
-                            onClick={console.log('Click en submit')}
+                            onClick={handleSubmit}
                         >
                             {disabled ? <CircularProgress size={24} /> : 'Guardar'}
                         </Button>
