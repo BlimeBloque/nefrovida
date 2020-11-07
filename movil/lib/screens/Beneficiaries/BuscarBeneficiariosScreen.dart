3d69b76c82b583d212031b3e5d7caab5f89a33b7
EricBL3 <ericbuitron3@gmail.com>
2020-11-07 14:32:09 -0600

    conectar api con la app para recibir lista de beneficiarios

--- movil/lib/screens/Beneficiaries/BuscarBeneficiariosScreen.dart
+++ movil/lib/screens/Beneficiaries/BuscarBeneficiariosScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:movil/classes/Beneficiario.dart';
+import 'package:movil/components/HttpHelper.dart';
 import 'package:movil/components/NefrovidaDrawer.dart';
 
 class BuscarBeneficiariosScreen extends StatelessWidget {
@@ -12,6 +14,47 @@ class BuscarBeneficiariosScreen extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: new NefrovidaDrawer(),
+      body: TablaBeneficiarios(),
+    );
+  }
+}
+
+class TablaBeneficiarios extends StatelessWidget {
+  TablaBeneficiarios({Key key}) : super(key: key);
+  HttpHelper benefHelper = new HttpHelper();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: FutureBuilder(
+        future: benefHelper.getAllBeneficiarios(),
+        builder: (BuildContext context, AsyncSnapshot snapshot){
+          if(snapshot.hasData != null)
+          {
+            List<Beneficiario> beneficiarios = snapshot.data;
+            if(beneficiarios == null)
+            {
+              return Center(child: CircularProgressIndicator());
+            }
+            else
+            {
+              return ListView.builder(
+                itemCount: beneficiarios.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(beneficiarios[index].nombreBeneficiario),
+                  );
+                },
+              );
+            }
+          }
+          else
+          {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
